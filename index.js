1fb54d5aee886eca0e70a6f0b5cdbd3da7e28821
Filipp Minayev <Filippmi@gmail.com>
2021-03-02 17:34:19 -0700

    commit tree 7013584c2af523a3be9dd111e2d17e6b64a38eb1

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Your code here
+let mapToNegativize = function(array) {
+  return array.map(x => x * -1)
+}
+
+let mapToNoChange = function(array) {
+  return array.map(x => x)
+}
+
+let mapToDouble = function(array) {
+  return array.map(x => x * 2)
+}
+
+let mapToSquare = function(array) {
+  return array.map(x => x * x)
+}
+
+let reduceToTotal = function(src, acc) {
+  return src.reduce((start, src) => start + src, acc)
+}
+
+
+// function reduceToTotal(src, startingPoint=0) {
+//   let total = startingPoint
+//   for (let i = 0; i < src.length; i++ ) {
+//     total = total + src[i]
+//   }
+//   return total
+// }
